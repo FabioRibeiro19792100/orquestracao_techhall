3d1ae826aa41008e166945a4992e435d27487d16
Fabio Ribeiro <fabiovagnerribeiro@gmail.com>
2026-08-25 20:50:46 -0300

    Add Vercel deployment build

---
+++ vercel-main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {createRoot} from "react-dom/client";
+import Home from "./app/page";
+import "./app/globals.css";
+import "./app/screens.css";
+import "./app/linear.css";
+import "./app/layers.css";
+import "./app/controls.css";
+import "./app/links.css";
+import "./app/integrated.css";
+import "./app/play.css";
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+);
